d342b552a8ac8964ca1a5e0dd860deaa075d6601
Brandon Gorson <42119471+Bgorson@users.noreply.github.com>
2018-12-24 12:34:31 -0600

    Attempting to use an Object for quiz questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,59 @@
+//Timer
+var time=30;
+var intervalId;
 
-function startGame() {
-    $(".start").remove();
+$("#start").on("click",function(){
+    start()
+    $("#start").remove();
     $("#timer").css("display","block")
     $('#questions').css("display","block")
-
-    
+    $('#countDown').html("Time Remaining: "+time+" Seconds")   
+})  
+ 
+function start () {
+    intervalId=setInterval(decrement,1000)
 }
+function decrement() {
+    time--;
+    $('#countDown').html("Time Remaining: "+time+" Seconds")
+    if (time === 0) {
+        // alert("Make the game do something else")
+        clearInterval(intervalId)
+    }
+}
+//Questions
+var quizSection =document.getElementById('questions');
+
+
+var quizQuestions = [
+    {
+        question: "Which one of these is NOT a Startcraft Race?",
+        answers: [
+            "Zerg",
+            "Protoss",
+            "Xel-Naga",
+            "Terran"
+        ],
+        correctAnswer:2
+    },
+        {
+            question: "Who is the main Protagonist?",
+            answers: [
+                "Sarah Kerrigan",
+                "Jim Raynor",
+                "Matt Horner",
+                "Murlocs"
+            ],
+            correctAnswer:1
+        },
+
+    ]
+
+    $(quizQuestions).each(function(i,e) {
+        $("#questions").append('<p>'+ e.question + '</p><label class= "radio-inline "answer="' + e.correctAnswer + '"><input type="radio" name="optradio">'+ e.answers[i] + '</label>')
+    }
+
+    )
 
 
 
